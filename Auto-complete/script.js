76d2b38f26287bb4b4a45c368b2f8fee10430b41
Deepak8717 <sendit2deepak@gmail.com>
2021-01-27 17:41:30 +0530

    auto-compelete form

---
+++ Auto-complete/script.js
@@ -0,0 +1,13 @@
+// Add the Javascript here for the function billingFunction(). It is responsible for setting and clearing the fields in Billing Information.
+function billingFunction() {
+  if (document.getElementById("same").checked) {
+    var shippingName = document.getElementById("shippingName").value;
+    var shippingZip = document.getElementById("shippingzip").value;
+
+    document.getElementById("billingName").value = shippingName;
+    document.getElementById("billingZip").value = shippingZip;
+  } else {
+    document.getElementById("billingName").value = "";
+    document.getElementById("billingZip").value = "";
+  }
+}
